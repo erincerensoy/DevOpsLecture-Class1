f5128a214d96b7b5e4d5941f03301f59dddaa95e
erincerensoy <erincerensoy@gmail.com>
2021-02-14 01:24:47 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator.js');
+
+console.log(calculator.add(3, 5));
+console.log(calculator.substract(13, 5));
+console.log(calculator.multiply(3, 5));
+console.log(calculator.divide(10, 5));
---
+++ calculator.js
@@ -0,0 +1,18 @@
+add = (a, b) => {
+    return a + b;
+}
+
+substract = (a, b) => {
+    return a - b;
+}
+
+multiply = (a, b) => {
+    return a * b;
+}
+
+divide = (a, b) => {
+    return a / b;
+}
+module.exports = {
+    add, substract, multiply, divide
+};
