b24aaab920680ac3bd1683cdd0ee6587c71b9644
erincerensoy <erincerensoy@gmail.com>
2021-03-28 01:25:13 +0300

    added duplication for sonr

--- src/routes.js
+++ src/routes.js
@@ -7,4 +7,9 @@ router.get('/add', (req, res) => {
   res.status(200).json({ result: additionResult });
 });
 
+router.get('/add', (req, res) => {
+  let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
+  res.status(200).json({ result: additionResult });
+});
+
 module.exports = router
