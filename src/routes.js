0d8d5e7b76bf4cc41482926a02cf2cefed875b87
erincerensoy <erincerensoy@gmail.com>
2021-04-14 17:50:46 +0300

    initialized

--- src/routes.js
+++ src/routes.js
@@ -3,30 +3,26 @@ const router = express.Router();
 const calculator = require('./calculator.js');
 
 
-router.get('/add', (req, res) => {
-  let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
-router.get('/substract', (req, res) => {
-  let additionResult = calculator.substract(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
-router.get('/divide', (req, res) => {
-  let additionResult = calculator.divide(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
-router.get('/multiply', (req, res) => {
-  let additionResult = calculator.multiply(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
 router.get('/', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
+
+router.get('/', (req, res) => {
+  let additionResult = calculator.substract(parseInt(req.query.a), parseInt(req.query.b));
+  res.status(200).json({ result: additionResult });
+});
+
+router.get('/', (req, res) => {
+  let additionResult = calculator.divide(parseInt(req.query.a), parseInt(req.query.b));
+  res.status(200).json({ result: additionResult });
+});
+
+router.get('/', (req, res) => {
+  let additionResult = calculator.multiply(parseInt(req.query.a), parseInt(req.query.b));
+  res.status(200).json({ result: additionResult });
+});
+
 
 add = (a, b) => {
   return a + b;
