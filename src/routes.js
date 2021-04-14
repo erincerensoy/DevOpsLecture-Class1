69cd7b6ff85508f3f83c289a4caef85e68f9f8c9
erincerensoy <erincerensoy@gmail.com>
2021-04-14 16:46:38 +0300

    initlialized

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const calculator = require('./calculator.js');
 
-router.get('/add', (req, res) => {
+router.get('/', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
