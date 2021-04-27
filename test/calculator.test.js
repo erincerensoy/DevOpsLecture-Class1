1fa6fcb70f25b518bbc0d11f240e318620a00175
erincerensoy <erincerensoy@gmail.com>
2021-04-27 23:52:58 +0300

    CAL-1 add method implemented

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,5 +3,9 @@ const assert = require('assert');
 const { add, substract, multiply, divide } = require('../src/calculator');
 
 describe("calculator", () => {
-
+    describe("adding 2 and 3", () => {
+        it("should return 5", () => {
+            assert.equal(add(2, 3), 5);
+        })
+    });
 })
