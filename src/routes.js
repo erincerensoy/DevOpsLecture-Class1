656c36ab8547afecf50b8d36ac559a33d52145ad
erincerensoy <erincerensoy@gmail.com>
2021-04-28 00:01:41 +0300

    initialize

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const calculator = require('./calculator.js');
 
 
-router.get('/add', (req, res) => {
+router.get('/', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,9 +3,4 @@ const assert = require('assert');
 const { add, substract, multiply, divide } = require('../src/calculator');
 
 describe("calculator", () => {
-    describe("adding 2 and 3", () => {
-        it("should return 5", () => {
-            assert.equal(add(2, 3), 5);
-        })
-    });
 })
