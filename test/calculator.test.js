3ad2ea788d179e886686e89d13fdac36ea0e0085
erincerensoy <erincerensoy@gmail.com>
2021-04-28 00:34:41 +0300

    added unit

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,4 +3,11 @@ const assert = require('assert');
 const { add, substract, multiply, divide } = require('../src/calculator');
 
 describe("calculator", () => {
+    describe("substraction", () => {
+        describe("substracting 5 from 40", () => {
+            it("should return 35", () => {
+                assert.equal(substract(40, 5), 35);
+            })
+        })
+    })
 })
