d9f9d02b6edf6fd91ad24686bfea90a93bb464de
erincerensoy <erincerensoy@gmail.com>
2021-04-28 18:49:39 +0300

    initialize

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const calculator = require('./calculator.js');
 
 
-router.get('/add', (req, res) => {
+router.get('/', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
 
-router.get('/substract', (req, res) => {
+router.get('/', (req, res) => {
   let additionResult = calculator.substract(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
 
-router.get('/divide', (req, res) => {
+router.get('/', (req, res) => {
   let additionResult = calculator.divide(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
