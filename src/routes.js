83a5987942c469ce5177ba127c12743386a08550
erinç erensoy <erincerensoy@erincs-MacBook-Pro.local>
2022-04-11 00:36:28 +0300

    addition :)

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const calculator = require('./calculator.js');
 
 
-router.get('/additionnn', (req, res) => {
+router.get('/addition', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
