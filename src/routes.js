21a22785803b94b679e2eb14617b6bd2fb9709c7
erinç erensoy <erincerensoy@erincs-MacBook-Pro.local>
2022-04-11 22:55:32 +0300

    init

--- src/routes.js
+++ src/routes.js
@@ -3,28 +3,13 @@ const router = express.Router();
 const calculator = require('./calculator.js');
 
 
-router.get('/additionn', (req, res) => {
+router.get('/addition', (req, res) => {
   let additionResult = calculator.add(parseInt(req.query.a), parseInt(req.query.b));
   res.status(200).json({ result: additionResult });
 });
 
-router.get('/substractt', (req, res) => {
-  let additionResult = calculator.substract(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
-router.get('/divide', (req, res) => {
-  let additionResult = calculator.divide(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
-router.get('/multiply', (req, res) => {
-  let additionResult = calculator.multiply(parseInt(req.query.a), parseInt(req.query.b));
-  res.status(200).json({ result: additionResult });
-});
-
 router.get('/healthcheck', (req, res) => {
-  res.status(200).json({ result: "healthy" });
+  res.status(200).json({ result: "very healthy" });
 });
 
 
