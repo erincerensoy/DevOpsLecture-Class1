87668dd93feec43d64f58f8b30e6ba414347c5cd
erinç erensoy <erincerensoy@ip-192-168-0-33.eu-west-1.compute.internal>
2022-04-13 21:44:01 +0300

    added divide method

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,11 @@ router.get('/substract', (req, res) => {
   res.status(200).json({ result: additionResult });
 });
 
+router.get('/divide', (req, res) => {
+  let additionResult = calculator.divide(parseInt(req.query.a), parseInt(req.query.b));
+  res.status(200).json({ result: additionResult });
+});
+
 router.get('/healthcheck', (req, res) => {
   res.status(200).json({ result: "very healthy" });
 });
