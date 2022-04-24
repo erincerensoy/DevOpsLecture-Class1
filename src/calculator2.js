4d58802be7316a2a6142f93525d7ec0435250725
erinç erensoy <erincerensoy@erincs-MacBook-Pro.local>
2022-04-25 01:22:28 +0300

    added duplications

---
+++ src/calculator2.js
@@ -0,0 +1,23 @@
+add = (a, b) => {
+    return a + b;
+}
+
+substract = (a, b) => {
+    return a - b;
+}
+
+multiply = (a, b) => {
+    return a * b;
+}
+
+multiply1 = (a, b) => {
+    return a * b;
+}
+
+divide = (a, b) => {
+    return a / b;
+}
+
+module.exports = {
+    add, substract, multiply, divide, multiply1
+};
