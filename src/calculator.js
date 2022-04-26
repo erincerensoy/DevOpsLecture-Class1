5c7146fd44fe4b9677496f0bc2fce7c4c160f0c8
erinç erensoy <erincerensoy@erincs-MacBook-Pro.local>
2022-04-26 23:58:05 +0300

    initialized

--- src/calculator.js
+++ src/calculator.js
@@ -2,18 +2,6 @@ add = (a, b) => {
     return a + b;
 }
 
-substract = (a, b) => {
-    return a - b;
-}
-
-multiply = (a, b) => {
-    return a * b;
-}
-
-divide = (a, b) => {
-    return a / b;
-}
-
 module.exports = {
-    add, substract, multiply, divide, square
+    add
 };
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,5 +1,5 @@
 const { describe } = require('mocha');
-const { add, substract, multiply, divide, square } = require('../src/calculator');
+const { add } = require('../src/calculator');
 var chai = require('chai')
 , assert = chai.assert
 , expect = chai.expect
